0b42202916ddb3ccc97dee75c83ce821902569d5
wspasindupanthaka <wspasindupanthaka>
2019-10-13 15:08:55 +0530

    recipe-list-component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','Test Description','https://media-cdn.tripadvisor.com/media/photo-s/0f/02/0f/2d/sri-lankan-authentic.jpg')
+  ];
 
   constructor() { }
 
